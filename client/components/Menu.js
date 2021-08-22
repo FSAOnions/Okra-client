8318f4c486b023206dda884e9779b910774b0aec
Lighein <Skiorline@gmail.com>
2021-08-21 22:08:31 -0400

    Gitignore file

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { StyleSheet, View, Button } from "react-native";
 import { ViroARSceneNavigator } from "react-viro";
-import ButtonComponent from "./ImageComponent";
 import { selectUserPage, setPage } from "../redux/reducers/userPage";
 import Hamburger from "./Hamburger";
 import MenuNav from "./MenuNav";
