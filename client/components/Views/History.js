2907bfeca5655c92e8078630ca65e0686980fe01
Lighein <Skiorline@gmail.com>
2021-09-03 16:24:24 -0400

    History fixed

--- client/components/Views/History.js
+++ client/components/Views/History.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   View,
   SafeAreaView,
+  TouchableOpacity,
   Image,
   ScrollView,
 } from "react-native";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import getDimensions from "../../util/getDimensions";
 import loadAsset from "../../util/loadAsset";
 loadAsset;
@@ -17,6 +18,8 @@ import { Text } from "@ui-kitten/components";
 
 export default function History() {
   const history = useSelector(getHistory);
+  const [ordersId, setOrderId] = useState(-1);
+
   return (
     <SafeAreaView style={styles.mainContainer}>
       <View
@@ -44,71 +47,73 @@ export default function History() {
         {history[0] &&
           history.map((info) => (
             <View
+              key={info.id}
               style={{
                 margin: 10,
                 marginLeft: windowWidth * 0.05,
                 marginRight: 10,
               }}
             >
-              <View
-                style={[
-                  styles.square,
-                  {
-                    shadowOffset: {
-                      width: 0,
-                      height: 0,
-                    },
-                    shadowOpacity: 0.2,
-                    shadowRadius: 10,
-                  },
-                ]}
-              >
+              <TouchableOpacity onPress={()=>{(ordersId!==info.id) ? setOrderId(info.id) : setOrderId(-1)}}>
+            
                 <View
-                  style={{
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    alignItems: "center"
-                  }}
+                  style={[
+                    styles.square,
+                    {
+                      shadowOffset: {
+                        width: 0,
+                        height: 0,
+                      },
+                      shadowOpacity: 0.3,
+                      shadowRadius: 7,
+                    },
+                  ]}
                 >
-                  <Image
-                    source={{ uri: loadAsset(`/${info.restaurant.imgUrl}`) }}
+                  <View
                     style={{
-                      width: windowWidth * 0.2,
-                      height: windowWidth * 0.2,
-                      margin: 10,
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
                     }}
-                  ></Image>
-                  <View style={{ width: windowWidth * 0.63 }}>
-                    <View
+                  >
+                    <Image
+                      source={{ uri: loadAsset(`/${info.restaurant.imgUrl}`) }}
                       style={{
-                        flexDirection: "row",
-                        marginTop: 3,
-                        justifyContent: "space-between",
+                        width: windowWidth * 0.2,
+                        height: windowWidth * 0.2,
+                        margin: 10,
                       }}
-                    >
-                      <Text
+                    ></Image>
+                    <View style={{ width: windowWidth * 0.63 }}>
+                      <View
                         style={{
-                          fontSize: 15,
-                          marginRight: 10,
-                          marginTop: 8,
-                          marginBottom: 5,
+                          flexDirection: "row",
+                          marginTop: 3,
+                          justifyContent: "space-between",
                         }}
                       >
-                        {info.restaurant.name}
-                      </Text>
-                      <Text
-                        style={{
-                          fontSize: 14,
-                          marginRight: 10,
-                          marginTop: 8,
-                          marginBottom: 5,
-                        }}
-                      >
-                        {info.createdAt.slice(0, 10)}
-                      </Text>
-                    </View>
-                    <View>
-                      {info.orders &&
+                        <Text
+                          style={{
+                            fontSize: 15,
+                            marginRight: 10,
+                            marginTop: 8,
+                            marginBottom: 5,
+                          }}
+                        >
+                          {info.restaurant.name}
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: 14,
+                            marginRight: 10,
+                            marginTop: 8,
+                            marginBottom: 5,
+                          }}
+                        >
+                          {info.createdAt.slice(0, 10)}
+                        </Text>
+                      </View>
+                      {info.orders && info.id===ordersId &&
                         info.orders.map((order) => (
                           <View>
                             {order.products.map((product) => (
@@ -139,41 +144,41 @@ export default function History() {
                             ))}
                           </View>
                         ))}
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          marginLeft: 13,
+                          marginRight: 10,
+                          marginBottom: 10,
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <Text
+                          style={{
+                            fontSize: 14,
+                          }}
+                        >
+                          Total:
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: 14,
+                          }}
+                        >
+                          {info.dollars}
+                        </Text>
+                      </View>
                     </View>
-                    <View
-                                style={{
-                                  flexDirection: "row",
-                                  marginLeft: 13,
-                                  marginRight: 10,
-                                  marginBottom: 10,
-                                  justifyContent: "space-between",
-                                }}
-                              >
-                                <Text
-                                  style={{
-                                    fontSize: 14,
-                                  }}
-                                >
-                                  Total: 
-                                </Text>
-                                <Text
-                                  style={{
-                                    fontSize: 14,
-                                  }}
-                                >
-                                  {info.dollars}
-                                </Text>
-                              </View>
                   </View>
                 </View>
-              </View>
+              </TouchableOpacity>
             </View>
           ))}
       </ScrollView>
     </SafeAreaView>
   );
 }
-
+ //(ordersArr.length ===0 ) ? setOrderArr(history[num].orders) : setOrderArr([])}
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
