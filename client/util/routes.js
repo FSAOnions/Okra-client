ea484b10264114e17dd76c7ffa9e7671139c2576
MoAdam10 <meedoadam@gmail.com>
2021-09-03 11:13:49 -0400

    added Bill summary and payment pages

--- client/util/routes.js
+++ client/util/routes.js
@@ -11,6 +11,7 @@ import Settings from "../components/Views/Settings";
 import Pending from "../components/Views/Pending";
 import ThankYou from "../components/Views/ThankYou";
 import Bill from "../components/Views/Bill";
+import Payment from "../components/Views/Payment";
 
 export default function getPage(link) {
   switch (link) {
@@ -34,6 +35,8 @@ export default function getPage(link) {
       return <Pending />;
     case "thankyou":
       return <ThankYou />;
+    case "payment":
+      return <Payment />;
     default:
       return <FourOhFour />;
   }
